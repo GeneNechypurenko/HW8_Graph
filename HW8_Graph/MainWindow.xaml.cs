040a3d5db215da40c7d60ba18b1f7d4fbc738c7c
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2023-12-05 15:22:29 +0200

    Add project files.

---
+++ HW8_Graph/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW8_Graph
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+           Point clickPoint = e.GetPosition(canvas);
+
+            if(clickPoint.X >= kitchenRect.Rect.Height && clickPoint.Y <= kitchenRect.Rect.Width )
+            {
+                MessageBox.Show("Кухня 18м2");
+            }
+            else if(clickPoint.X >= coridorRect.Rect.Height && clickPoint.Y <= coridorRect.Rect.Width)
+            {
+                MessageBox.Show("Коридор 20м2");
+            }
+            else if (clickPoint.X >= bedroomRect.Rect.Height && clickPoint.Y <= bedroomRect.Rect.Width)
+            {
+                MessageBox.Show("Спальния 23м2");
+            }
+            else if (clickPoint.X >= hallRect.Rect.Height && clickPoint.Y <= hallRect.Rect.Width)
+            {
+                MessageBox.Show("Зал 34м2");
+            }
+        }
+    }
+}
